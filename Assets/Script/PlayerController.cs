49328d9d456b235068f1384e6fd7c39723b53d46
JohnyCornejo <JoCornejoFlores@s.sfusd.edu>
2023-03-14 13:59:01 -0700

    Main

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -31,7 +31,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Powerup"))
+        if(other.CompareTag("PowerUp"))
         {
             hasPowerup = true; 
             Destroy(other.gameObject); 
